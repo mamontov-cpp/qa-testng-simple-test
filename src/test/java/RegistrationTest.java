881b9e2fa2b4919e46d035ef846068b63b301b72
Mamontov Dmitry <mamontov.dp@gmail.com>
2019-03-19 16:03:17 +0400

    Added first test stub.

---
+++ src/test/java/RegistrationTest.java
@@ -0,0 +1,16 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+/**
+ *  A basic test for registration page
+ */
+@Test()
+public class RegistrationTest {
+
+    @Test()
+    public void test()
+    {
+        Assert.assertEquals("1", "1");
+    }
+}
